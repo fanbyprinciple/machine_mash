f5deb72f00e918c3b8a373baa7f132f7555b66d7
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-26 23:22:57 +0530

    transferLearning: created with issues

--- transferLearning/sketch.js
+++ transferLearning/sketch.js
@@ -1,10 +1,40 @@
 let mobileNet
 let video
-let label = " Camera not inititalised."
+let label = " Model not initialised ."
+let planeButton
+let notPlaneButton
+let trainBUtton
 
 function modelReady(){
   console.log("Model loaded.")
 }
+
+function videoReady(){
+  console.log("Video is ready.")  
+
+}
+
+function whileTraining(loss){
+  if(loss == null){
+    console.log('Training complete.')
+    classifier.classify(gotResults)
+  } else {
+    console.log(loss)
+  }
+
+}
+
+function gotResults(error, result){
+  if (error){
+    console.log("Classifier could not execute. Check the internet")
+  } else {
+    console.log(result)
+    label = result[0].label
+    classifier.classify(gotResults)
+  }
+}
+
+
 
 function setup(){
   createCanvas(640,600)
@@ -14,6 +44,22 @@ function setup(){
   video.hide()
 
   mobilenet = ml5.featureExtractor('MobileNet', modelReady)
+  classifier = mobilenet.classification(video, videoReady)
+
+  planeButton = createButton('plane')
+  planeButton.mousePressed(function(){
+    classifier.addImage('plane')
+  })
+
+  notPlaneButton = createButton('notPlane')
+  notPlaneButton.mousePressed(function(){
+    classifier.addImage('notPlane')
+  })
+
+  trainButton = createButton('train')
+  trainButton.mousePressed(function(){
+    classifier.train(whileTraining);
+  })
 }
 
 function draw() {
