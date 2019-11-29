e56231f4ba1a0ee3b9f7ea9d62d68b33604fcdb6
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-11-30 00:42:50 +0530

    bikefat: changes made

--- bikefast/ml_model.py
+++ bikefast/ml_model.py
@@ -16,3 +16,16 @@ print(data.head())
 data['dayofyear'] = (data['dteday'] - data['dteday'].apply(lambda x : date (x.year,1,1)).astype('datetime64[ns]')).apply(lambda x: x.days)
 
 offset = int(len(data)*0.8)
+
+X = np.array(data.drop(['dteday', 'mnth', 'casual', 'registered', 'cnt'], axis=1))
+Y = np.array(data['cnt'])
+
+X_train, X_test = X[:offset], X[offset:]
+Y_train, Y_test = Y[:offset], Y[offset:]
+
+RF = RandomForestRegressor()
+
+RF.fit(X_train,Y_train)
+
+with open("modelfile.pickle", 'wb') as handle :
+    pickle.dump(RF, handle,protocol=pickle.HIGHEST_PROTOCOL)
--- bikefast/run_server.py
+++ bikefast/run_server.py
@@ -1,16 +1,37 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+from return_feature import  doTheCalculation
+
+import json, pickle
+import pandas as pd 
+
+import numpy as np   
+
 app = Flask(__name__)
-@app.route('/api/makeplc/', methods=['POST'])
+
+@app.route('/api/makecalc/', methods=['POST'])
 
 def makecalc():
 
     jsonfile = request.get_json()
+
+    data = pd.read_json(json.dumps(jsonfile), orient='index', convert_dates=['dteday'])
     
+    print(data)
+
+    res = dict()
+
+    ypred = model.predict(doTheCalculation(data))
+
+    for i in range(len(ypred)):
+        res[i] = ypred[i]
+
+
     return jsonify(res)
 
 
 if __name__ == '__main__':
 
-    model = pickle.load(open('modelfile', 'rb'))
+    model = pickle.load(open('modelfile.pickle', 'rb'))
     app.run(debug=True)
 
     
